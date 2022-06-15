dc2b6d2f69e88437ecde0ce0976bbf678a107396
andy <andersgee@gmail.com>
2022-06-15 11:09:33 +0200

    hide mongodb logs

--- apis/sideprojects/src/mongodb.ts
+++ apis/sideprojects/src/mongodb.ts
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 
 export async function mongooseConnection(mongodbUrl?: string) {
   if (!mongodbUrl) throw new Error("no mongodb url");
-  console.log(`mongoose connecting to mongodb: ${mongodbUrl} ...`);
+  console.log(`mongoose waiting for connection to: ${mongodbUrl} ...`);
 
-  mongoose.connection.once("open", () => console.log("... mongoose connected to mongodb"));
-  mongoose.connection.on("error", (err) => console.log(`... mongoose connection error`, err));
+  mongoose.connection.once("open", () => console.log(`... mongoose connected to: ${mongodbUrl}`));
+  mongoose.connection.on("error", (err) => console.log(`... ERROR connecting to mongodb:`, err));
   await mongoose.connect(mongodbUrl);
 }
